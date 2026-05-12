f11218a0cc835711c3dce1f410a3402cf69b8083
Aditya kumar singh <singhaditya10311@gmail.com>
2026-05-12 16:34:51 +0530

    Update uploadRoutes.js

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -1,24 +1,17 @@
-import express from "express";
+import express from "express"
+import { protect } from "../middleware/authMiddleware.js"
+import { authorizeRoles } from "../middleware/roleMiddleware.js"
+import upload from "../middleware/uploadMiddleware.js"
+import { uploadDocuments } from "../controllers/uploadController.js"
 
-import { protect } from "../middleware/authMiddleware.js";
-import { authorizeRoles } from "../middleware/roleMiddleware.js";
+const router = express.Router()
 
-import upload from "../middleware/uploadMiddleware.js";
-
-import {
-  uploadDocuments,
-} from "../controllers/uploadController.js";
-
-const router = express.Router();
-
-
-// Upload caregiver docs
 router.post(
   "/documents",
   protect,
   authorizeRoles("caregiver"),
   upload.array("documents", 5),
   uploadDocuments
-);
+)
 
-export default router;
+export default router
